3e977a332153e6fff5950a50ff06efbf099dd192
Nicolai Amirzadeh <nicolai.amirzadeh@outlook.com>
2021-08-16 13:50:05 +0200

    Creating a class for Drum kit, and contstructor for pads & Audio

---
+++ app.js
@@ -0,0 +1,8 @@
+class DrumKit {
+    constructor() {
+        this.pads = document.querySelectorAll(".pad");
+        this.kickAudio = document.querySelector(".kick-sound");
+        this.clapAudio = document.querySelector(".clap-sound");
+        this.hihatAudio = document.querySelector(".hihat-sound");
+    }
+}
