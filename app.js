c5bb7ca3b0bc3fc62157a4d8fd48efe6a4ebf4d1
Nicolai Amirzadeh <nicolai.amirzadeh@outlook.com>
2021-08-17 20:05:00 +0200

    Resets sound for each pad

--- app.js
+++ app.js
@@ -28,12 +28,15 @@ class DrumKit {
             if(bar.classList.contains("active")) {
                 // Check each sound
                 if(bar.classList.contains("kick-pad")) {
+                        this.kickAudio.currentTime = 0;
                     this.kickAudio.play();
                 }
                 if(bar.classList.contains("clap-pad")) {
+                        this.clapAudio.currentTime = 0;
                     this.clapAudio.play();
                 }
                 if(bar.classList.contains("hihat-pad")) {
+                        this.hihatAudio.currentTime = 0;
                     this.hihatAudio.play();
                 }
             }
